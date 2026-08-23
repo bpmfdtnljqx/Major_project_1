7ceb905eb6b8073a639be40a9e10016a3cf6a61a
Liu Zehui <2932598628@qq.com>
2026-08-23 20:36:20 +0800

    fix:编写client.py

---
+++ crawler/client.py
@@ -0,0 +1,26 @@
+import requests
+from crawler.settings import(
+    SEARCH_API,
+    DEFAULT_PAGESIZE,
+    TIME_OUT,
+    HEADERS
+)
+
+def get_songs(keyword, page):
+    #请求参数
+    Params={
+        "text": keyword,
+        "pageNo": page,
+        "pageSize": DEFAULT_PAGESIZE
+    }
+    #获取响应数据
+    response=requests.get(
+        SEARCH_API,
+        params= Params,
+        headers= HEADERS,
+        timeout= TIME_OUT
+    )
+
+    response.raise_for_status()
+
+    return response.json()
