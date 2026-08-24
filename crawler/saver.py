4a3363a761c7e6b9cb6ee0f43e19859285e1b2dd
Liu Zehui <2932598628@qq.com>
2026-08-24 14:17:04 +0800

    feat:编写saver.py

---
+++ crawler/saver.py
@@ -0,0 +1,10 @@
+import json
+
+def save_json(data,file):
+    with open(file, "w", encoding= "utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii= False,
+            indent= 4
+        )
