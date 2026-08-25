c72448702e546fef16c1dea0239943dc284349b4
Liu Zehui <2932598628@qq.com>
2026-08-25 12:12:13 +0800

    fix:根据网易云网站修改的settings和client

--- crawler/client.py
+++ crawler/client.py
@@ -1,8 +1,9 @@
 import requests
 from crawler.settings import(
     SEARCH_API,
-    ARTIST_API,
+    LYRIC_API,
     DEFAULT_PAGESIZE,
+    COOKIE,
     TIME_OUT,
     HEADERS
 )
@@ -10,16 +11,25 @@ from crawler.settings import(
 def get_songs(keyword, page):
     """从网站得到歌曲"""
     #请求参数
-    Params={
-        "text": keyword,
-        "pageNo": page,
-        "pageSize": DEFAULT_PAGESIZE
+    data={
+        "PARAMS" : ("9jfcU6fwrks/3++gMPqNYDC6yIs3dk7nNKjWbx"
+        "BK1EDTC5RcxylAt32Mf08QZ2hv7sBM7BwnIiCVs0z"
+        "y8+sqx9oFBenzslT9BOfcHc485xfSlwKeUOxzY7zptnH"
+        "HmRr92I79Y+WIjCw/lwNx0jLqesnYtKnUd4QuPOcbxHn"
+        "O/jZDTeRFVXUXABb9rKbmhALAo/tkR4Q3PoP0RQ74XCwkW5"
+        "2mANOSYBJjPwUPTLsoToFQrZ1+tU0H8dverENqjmu3"
+        "loJ5Yd6oAfrN/RXavSOjDvWoBA9J3wf+fdAgDhL5DC29VgyS7lNGzkF/XqMzzBzx"),
+        "ENCSECKEY" : ("4ec7e6836b4657f51b214177b737c93e164af898cdda498ec28"
+        "e8f4096ed6ca10aa624dad506fb436de27e77a1e7c5dccdd7b88e183b5733b1d7200b9f901705"
+        "0f604dff67fe1028c3445ee4a3db5ffc72e12041a08ab6"
+        "0e5503cb025cc0cf5f2a882bc2b73b308d9c9427171012cd9d12a167b8a04f86a087349f45a838a125")
     }
     #获取响应数据
-    response=requests.get(
+    response=requests.post(
         SEARCH_API,
-        params= Params,
+        data = data,
         headers= HEADERS,
+        cookies=COOKIE,
         timeout= TIME_OUT
     )
 
---
+++ crawler/test.py
@@ -0,0 +1,10 @@
+from crawler.client import get_songs
+
+
+result = get_songs(
+    "林俊杰",
+    1
+)
+
+
+print(result)
