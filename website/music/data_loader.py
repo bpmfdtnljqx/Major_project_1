2de07bc7e3937bc568696b8f7428188884d375d0
Liu Zehui <2932598628@qq.com>
2026-08-29 21:55:00 +0800

    fix:增加缓存设置提高搜索效率

--- website/music/data_loader.py
+++ website/music/data_loader.py
@@ -5,12 +5,22 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DATA_DIR = BASE_DIR/"data"/"raw"
 
+#为搜索做优化
+songs_cache = None
+artists_cache = None
+
 def load_songs():
-    file_path = DATA_DIR/"song.json"
-    with open(file_path,"r",encoding="utf-8") as file:
-        return json.load(file)
+    global songs_cache
+    if songs_cache is None:
+        file_path = DATA_DIR/"song.json"
+        with open(file_path,"r",encoding="utf-8") as file:
+            songs_cache = json.load(file)
+    return songs_cache
 
 def load_artists():
-    file_path = DATA_DIR/"artist.json"
-    with open(file_path,"r",encoding="utf-8") as file:
-        return json.load(file)
+    global artists_cache
+    if artists_cache is None:
+        file_path = DATA_DIR/"artist.json"
+        with open(file_path,"r",encoding="utf-8") as file:
+            artists_cache = json.load(file)
+    return artists_cache
